e981e1e0c7c145a0278aa3032a41da99640c00bc
shashwotghimire <shashwot234@gmail.com>
2026-02-28 21:21:19 +0545

    Update pagination button styles to improve accessibility and user experience

--- app/web/components/accounts-page/pagination-button.tsx
+++ app/web/components/accounts-page/pagination-button.tsx
@@ -11,7 +11,7 @@ const PaginationButton = ({
     <div>
       <button
         onClick={onClick}
-        className={`border-0.5 cursor-pointer rounded-lg p-3 shadow-sm transition ${disabled ? "cursor-not-allowed bg-neutral-600" : "cursor-pointer bg-neutral-300 hover:bg-neutral-500"}`}
+        className={`border-0.5 cursor-pointer rounded-lg p-3 shadow-sm transition ${disabled ? "text-muted-foreground cursor-not-allowed bg-neutral-600" : "cursor-pointer bg-neutral-300 hover:bg-neutral-500"}`}
         disabled={disabled}
       >
         {pageNumber}
